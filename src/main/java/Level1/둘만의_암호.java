8a8bf9b705c7baa1543468e4515dc051b089f9bd
username459712 <94822739+username459712@users.noreply.github.com>
2023-02-03 20:45:18 +0900

    둘만의 암호

--- src/main/java/Level1/둘만의_암호.java
+++ src/main/java/Level1/둘만의_암호.java
@@ -6,19 +6,26 @@ public class 둘만의_암호 {
     }
 
     public static String solution(String s, String skip, int index) {
+        String answer = "";
+
         char[] data = s.toCharArray();
-        for(int i = 0 ; i < data.length ; i++){
-            for(int j = 0 ; j<index; j++){
-                do{
+        for(int i=0; i<data.length; i++){
+            for (int j=0; j<index; j++){
+                data[i]++;
+                if(data[i] > 'z'){
+                    data[i] -= 26;
+                }
+                while (skip.contains(String.valueOf(data[i]))){
                     data[i]++;
                     if(data[i] > 'z'){
                         data[i] -= 26;
                     }
-                }while(skip.contains(String.valueOf(data[i])));
+                }
             }
         }
-        String answer = String.valueOf(data);
-        return answer;
 
+        answer = String.valueOf(data);
+
+        return answer;
     }
 }
